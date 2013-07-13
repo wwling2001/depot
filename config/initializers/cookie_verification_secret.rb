28737bef12d873957bfc69f4084b1c05df78a2be
huangwl <huangwl@linux-b38m.(none)>
2013-07-13 02:30:14 -0400

    add depot source code

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '39fd15e54f3b1a4bf16ce57e26c608f81633961bd243ba286c531ac8aeca9b912bdb055ca1df529edf2c82d5a693179e9e0ba80ae1a2967989ed8cff8a9b88ee';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '01be2add88698fe87c886d66600b12025fa5ec997e486f35a406e9a0e8ff76cc6c6794ccd1512c383228031bcd78805d22854e143dc2e7d318ec34ce54354e67'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+ActionController::Base.session_store = :active_record_store
